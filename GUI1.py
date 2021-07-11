b6aa6c791560ae33a1edebdb5db3db99bbda9f36
Novfensec <87070473+Novfensec@users.noreply.github.com>
2021-07-11 18:58:23 +0530

    Update GUI1.py

--- GUI1.py
+++ GUI1.py
@@ -1,4 +1,15 @@
 from Tkinter import *
 root=Tk()
-root.geometry=('987x456')
+#window Width x Height
+root.geometry('987x456')
+
+#window maximum size
+root.minsize(787,256)
+
+#window minimum size
+root.maxsize(987,456)
+
+#add text to GUI window
+Label(root,text="This is the first Label in this GUI! Please subscribe!")
+
 root.mainloop()
