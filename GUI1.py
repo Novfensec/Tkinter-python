0d99db7f9a80c1480be768d2bd5c4e3cc54afb21
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2021-07-23 08:14:34 +0530

    Update GUI1.py

--- GUI1.py
+++ GUI1.py
@@ -9,13 +9,30 @@ root=Tk()
 #window default size Width x Height
 root.geometry('987x456')
 
+#window title
+root.title("Untitled-Notepad")
+
 #window maximum size
 root.minsize(787,256)
 
 #window minimum size
 root.maxsize(987,456)
 
-#add text to GUI window
+
+
+#window Icon choice1
+img=PhotoImage(file="filename")
+root.iconphoto(False,img)
+
+#window Icon choice2 (recommended bitmaps)
+root.iconbitmap(r'filename.ico')
+
+#window Icon choice3 (recommended bitmaps)[best way]
+root.wm_iconbitmap("filename.ico")
+
+
+
+#add text to GUI window with Label widget
 txt=Label(root,text="This is the first Label in this GUI! Please subscribe!")
 text.pack()
 
